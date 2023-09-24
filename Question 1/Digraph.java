57c0359253609fe97bbf9cad0d8c3d8a35135a24
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-09-24 12:58:03 +0300

    Update Digraph.java

--- Question 1/Digraph.java
+++ Question 1/Digraph.java
@@ -1,19 +1,12 @@
-package Assignment2_Q1;
-import Assignment1_Q1.Bag;
-
 public class Digraph {
 
 
 	//-----------------------------------------------------------------
 	// Title: Question 1
-	// Author: Basme Zantout
-	// ID: 99227947762
-	// Section: 1
-	// Assignment: 2
 	// Description: This is an Directed Graph class, which has the 
-    //              methods that create a directed graph and that perform
-    //              basic graph operations based on the Graph data structure.
-    //              This class also depends on the Bag.java class in its
+        //              methods that create a directed graph and that perform
+        //              basic graph operations based on the Graph data structure.
+        //              This class also depends on the Bag.java class in its
 	//              implementation
 	//-----------------------------------------------------------------
 	
